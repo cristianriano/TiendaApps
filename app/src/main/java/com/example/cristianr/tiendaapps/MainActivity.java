5ec7cc3af12343b9fb71840d2b279caeddf16fba
Cristian Riaño <cristiancarm11@gmail.com>
2017-01-13 18:53:53 -0500

    Initialize repo

---
+++ app/src/main/java/com/example/cristianr/tiendaapps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cristianr.tiendaapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
