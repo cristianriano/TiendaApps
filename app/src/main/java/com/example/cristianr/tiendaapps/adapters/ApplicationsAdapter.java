93e7593611c795b0b7420482179b2bdf68086adf
Cristian Riaño <cristiancarm11@gmail.com>
2017-01-19 15:11:40 -0500

    Add touch feedback to ViewCard

--- app/src/main/java/com/example/cristianr/tiendaapps/adapters/ApplicationsAdapter.java
+++ app/src/main/java/com/example/cristianr/tiendaapps/adapters/ApplicationsAdapter.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.widget.RecyclerView;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
